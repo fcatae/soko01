338a603f29254432db4236d7bd665bcea86279f1
fcatae <fcatae@gmail.com>
2014-06-17 15:39:11 -0300

    sound

--- Soko02/input.js
+++ Soko02/input.js
@@ -2,27 +2,41 @@
 window.addEventListener("keydown", function (e) {
 
     var key = e.keyCode;
+    var px = playerX;
+    var py = playerY;
 
     switch (key) {
         case 37:
         case 65: // left
-            playerX = playerX - 1;
+            px = px - 1;
             break;
 
         case 38:
         case 87: // up
-            playerY = playerY - 1;
+            py = py - 1;
             break;
 
         case 39:
         case 68: // right
-            playerX = playerX + 1;
+            px = px + 1;
             break;
 
         case 40:
         case 83: // down
-            playerY = playerY + 1;
+            py = py + 1;
             break;
+    }
+
+    var next = map[py][px];
+
+    if (next != '#') {
+        playerX = px;
+        playerY = py;
+    }
+
+    if (next == '$') {
+        sndcoins.play();
+        map[py][px] = ' '; // broken -- because strings are immutable
     }
 
 });
