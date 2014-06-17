4d6e81fbd85f6c63ce175534aeeb74330f45075a
fcatae <fcatae@gmail.com>
2014-06-17 15:00:00 -0300

    gameloop, sprites e input

---
+++ Soko02/game.js
@@ -0,0 +1,40 @@
+﻿
+var context = canvas.getContext("2d");
+
+var playerX = 1;
+var playerY = 1;
+
+window.onload = function () {
+
+    setInterval(function () {
+
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        drawBrick(0, 0);
+        drawBrick(1, 0);
+        drawBrick(2, 0);
+        drawBrick(3, 0);
+        drawBrick(4, 0);
+
+        drawPlayer(playerX, playerY);
+
+    }, 100);
+
+};
+
+function drawBrick(x, y) {
+    drawImage(imgbrick, x, y);
+}
+
+function drawPlayer(x, y) {
+    var image = imgface1;
+    drawImage(image, x, y);
+}
+
+function drawImage(image,x,y)
+{
+    context.drawImage(image,
+        0, 0, image.width, image.height,
+        x * image.width, y * image.width, image.width, image.height);
+
+}
---
+++ Soko02/input.js
@@ -0,0 +1,28 @@
+﻿
+window.addEventListener("keydown", function (e) {
+
+    var key = e.keyCode;
+
+    switch (key) {
+        case 37:
+        case 65: // left
+            playerX = playerX - 1;
+            break;
+
+        case 38:
+        case 87: // up
+            playerY = playerY - 1;
+            break;
+
+        case 39:
+        case 68: // right
+            playerX = playerX + 1;
+            break;
+
+        case 40:
+        case 83: // down
+            playerY = playerY + 1;
+            break;
+    }
+
+});
